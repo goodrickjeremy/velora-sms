5a43e98a06faa6a3c24dd2e2fd492b113e7bd6cd
goodrickjeremy <jeremy@velorabiolabs.com>
2026-04-10 00:12:38 -0600

    Update shopify-order.js

--- api/shopify-order.js
+++ api/shopify-order.js
@@ -11,7 +11,7 @@ export default async function handler(req, res) {
       await client.messages.create({
         body: 'New Shopify order received!',
         from: process.env.TWILIO_PHONE,
-        to: 'YOUR_PHONE_NUMBER'
+        to: '3074318109'
       });
 
       return res.status(200).json({ success: true });
