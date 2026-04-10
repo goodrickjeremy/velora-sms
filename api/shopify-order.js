219202d9dfe655fca9759cd62268791670d9f952
goodrickjeremy <jeremy@velorabiolabs.com>
2026-04-09 23:51:49 -0600

    Create shopify-order.js

---
+++ api/shopify-order.js
@@ -0,0 +1,9 @@
+export default async function handler(req, res) {
+  if (req.method === 'POST') {
+    console.log('Order received:', req.body);
+
+    return res.status(200).json({ success: true });
+  }
+
+  return res.status(405).json({ message: 'Method not allowed' });
+}
